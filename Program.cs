e23c870d9ca2ef6f1569847924633f706110cc6d
Teodor Kazakov <110903137+TedKazakov@users.noreply.github.com>
2022-08-09 14:23:46 +0200

    Proba edno

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nWagons = int.Parse(Console.ReadLine());
+            int[] peopleOnEachWagon = new int[nWagons];
+            int sum = 0;
+            for (int i = 0; i < peopleOnEachWagon.Length; i++)
+            {
+                peopleOnEachWagon[i] = int.Parse(Console.ReadLine());
+                sum += peopleOnEachWagon[i];
+            }
+            Console.WriteLine(string.Join(" ", peopleOnEachWagon));
+            Console.WriteLine(sum);
+        }
+    }
+}
