81a8df638333115f1c58e809508b8f0cbe1c5f0a
Teodor Kazakov <kazakovtedikazakov28@gmail.com>
2022-09-02 22:46:37 +0200

    Last commit here

---
+++ Fundamentals/01. Intro and Basic Syntax/Exercise/2. Divison/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _2._Divison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //2 or 3,  or 6, or 7, or 10
+            int numToDevide = int.Parse(Console.ReadLine());
+            int max = 0;
+            //by 2 or 3,  or 6, or 7, or 10 
+            if (numToDevide % 2 == 0)
+            {
+                max = 2;
+            }
+            if (numToDevide % 3 == 0)
+            {
+                max = 3;
+            }
+            if (numToDevide % 6 == 0)
+            {
+                max = 6;
+            }
+            if (numToDevide % 7 == 0)
+            {
+                max = 7;
+            }
+            if (numToDevide % 10 == 0)
+            {
+                max = 10;
+            }
+            if (max == 0)
+            {
+                Console.WriteLine("Not divisible");
+                return;
+            }
+            Console.WriteLine($"The number is divisible by {max}");
+        }
+    }
+}
---
+++ Fundamentals/01. Intro and Basic Syntax/Exercise/7. Vending Machine/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace _7._Vending_Machine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string acceptCoinString = Console.ReadLine();
+            double balance = 0;
+            while (acceptCoinString != "Start")
+            {
+                double coin = double.Parse(acceptCoinString);
+
+                if (coin == 0.1 || coin == 0.2 || coin == 0.5 || coin == 1 || coin == 2)
+                {
+                    balance += coin;
+
+                }
+                else Console.WriteLine($"Cannot accept {coin}");
+                acceptCoinString = Console.ReadLine();
+
+            }
+
+            //•	"Nuts" with a price of 2.0
+            //•	"Water" with a price of 0.7
+            //•	"Crisps" with a price of 1.5
+            //•	"Soda" with a price of 0.8
+            //•	"Coke" with a price of 1.0
+            double price = 0;
+            string product = Console.ReadLine();
+            while (product != "End")
+            {
+                switch (product)
+                {
+                    case "Nuts":
+                        {
+                            price = 2;
+                            if (balance < price)
+                            {
+                                Console.WriteLine("Sorry, not enough money");
+                                product = Console.ReadLine();
+                                continue;
+                            }
+                            Console.WriteLine($"Purchased nuts");
+                            balance -= price;
+                        }
+                        break;
+                    case "Water":
+                        {
+                            price = 0.7;
+                            if (balance < price)
+                            {
+                                Console.WriteLine("Sorry, not enough money");
+                                product = Console.ReadLine();
+                                continue;
+                            }
+                            Console.WriteLine($"Purchased water");
+                            balance -= price;
+                        }
+                        break;
+                    case "Crisps":
+                        {
+                            price = 1.5;
+                            if (balance < price)
+                            {
+                                Console.WriteLine("Sorry, not enough money");
+                                product = Console.ReadLine();
+                                continue;
+                            }
+                            Console.WriteLine($"Purchased crisps");
+                            balance -= price;
+                        }
+                        break;
+                    case "Soda":
+                        {
+                            price = 0.8;
+                            if (balance < price)
+                            {
+                                Console.WriteLine("Sorry, not enough money");
+                                product = Console.ReadLine();
+                                continue;
+                            }
+                            Console.WriteLine($"Purchased soda");
+                            balance -= price;
+                        }
+                        break;
+                    case "Coke":
+                        {
+                            price = 1;
+                            if (balance < price)
+                            {
+                                Console.WriteLine("Sorry, not enough money");
+                                product = Console.ReadLine();
+                                continue;
+                            }
+                            Console.WriteLine($"Purchased coke");
+                            balance -= price;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Invalid product");
+                        break;
+                }
+                product = Console.ReadLine();
+
+            }
+            Console.WriteLine($"Change: {balance:f2}");
+
+        }
+    }
+}
